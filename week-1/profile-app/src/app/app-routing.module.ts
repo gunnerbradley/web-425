fb948d16a9fe30d8ee90811b8d36ae207be84834
Gunner Bradley <gunnerbradley.dev@gmail.com>
2022-01-16 10:01:05 -0600

    exercise 2.2- Nav

--- week-1/profile-app/src/app/app-routing.module.ts
+++ week-1/profile-app/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component.ts';
+import { ContactComponent } from './contact/contact.component.ts';
+import { AboutComponent } from './about/about.component.ts';
+
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
