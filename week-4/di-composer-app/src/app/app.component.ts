585d9091a91e468056258d9fb9f48c7fef507f6e
Gunner Bradley <gunnerbradley.dev@gmail.com>
2022-01-28 14:51:38 -0600

    Exercise 4.3 - Handling Form Events with Observables

---
+++ week-4/di-composer-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'enhanced-composer-app';
+  assignment = 'Exercise 3.2 - Passing Data to Routes, Part 1';
+}
---
+++ week-4/reactive-composer-app/src/app/composer.service.ts
@@ -0,0 +1,58 @@
+/**
+ * Title: composer.service.ts
+ * Author: Gunner BRadley
+ * Date: January 28 2021
+ * Description: Service class for Composer objects
+ */
+
+import { IComposer } from './composer.interface';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ComposerService {
+  composers: Array<IComposer>;
+
+  constructor() {
+    this.composers = [
+      {
+        composerId: 100,
+        fullName: 'John Williams',
+        genre: 'Cinematic',
+      },
+      {
+        composerId: 101,
+        fullName: 'Johann Sebastian Bach',
+        genre: 'Classical',
+      },
+      {
+        composerId: 102,
+        fullName: 'Wolfgang Amadeus Mozart',
+        genre: 'Classical',
+      },
+      {
+        composerId: 103,
+        fullName: 'Johannes Brahms',
+        genre: 'Classical',
+      },
+      {
+        composerId: 104,
+        fullName: 'Joseph Haydn',
+        genre: 'Classical',
+      },
+    ];
+  }
+
+  getComposers() {
+    return this.composers;
+  }
+
+  getComposer(composerId: number) {
+    for (let composer of this.composers) {
+      if (composer.composerId === composerId) {
+        return composer;
+      }
+    }
+  }
+}
