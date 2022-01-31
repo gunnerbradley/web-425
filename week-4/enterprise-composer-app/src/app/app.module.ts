9f52172ca7dd8aef2ea13c90b4e261626d99bcfd
Gunner Bradley <gunnerbradley.dev@gmail.com>
2022-01-30 17:42:39 -0700

    Assignment 4.4

---
+++ week-4/enterprise-composer-app/src/app/app.module.ts
@@ -0,0 +1,31 @@
+/**
+ * Title: app.module.ts
+ * Author: Gunner Bradley
+ * Date: January 28 2022
+ * Description: App module
+ */
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ComposerListComponent } from './composer-list/composer-list.component';
+import { ComposerDetailsComponent } from './composer-details/composer-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    AboutComponent,
+    ComposerListComponent,
+    ComposerDetailsComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ week-4/enterprise-composer-app/src/app/composer.service.ts
@@ -0,0 +1,59 @@
+/**
+ * Title: composer.service.ts
+ * Author: Gunner Bradley
+ * Date: January 29th 2022
+ * Description: Service class for Composer objects
+ */
+
+import { Injectable } from '@angular/core';
+import { IComposer } from './composer.interface';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ComposerService {
+  composers: Array<IComposer>;
+
+  constructor() {
+    this.composers = [
+      { composerId: 100, fullName: 'John Williams', genre: 'Cinematic' },
+      {
+        composerId: 101,
+        fullName: 'Johann Sebastian Bach',
+        genre: 'Classical',
+      },
+      {
+        composerId: 102,
+        fullName: 'Wolfgang Amadeus Mozart',
+        genre: 'Classical',
+      },
+      { composerId: 103, fullName: 'Johannes Brahms', genre: 'Classical' },
+      { composerId: 104, fullName: 'Joseph Haydn', genre: 'Classical' },
+    ];
+  }
+
+  getComposers(): Observable<IComposer[]> {
+    return of(this.composers);
+  }
+
+  getComposer(composerId: number) {
+    for (let composer of this.composers) {
+      if (composer.composerId === composerId) {
+        return composer;
+      }
+    }
+  }
+
+  filterComposers(name: string): Observable<IComposer[]> {
+    return of(this.composers).pipe(
+      map((composers) =>
+        composers.filter(
+          (composer) => composer.fullName.toLowerCase().indexOf(name) > -1
+        )
+      )
+    );
+  }
+}
