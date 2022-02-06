9a6c3c9fa5b8d2105f5312dece7961912b76952c
Gunner Bradley <gunnerbradley.dev@gmail.com>
2022-02-06 14:44:03 -0600

    Exercise 5.2

---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,19 @@
+/**
+ * Title: book-list.component
+ * Author: Gunner Bradley
+ * Date: February 6th 2022
+ * Description: Book list component (Book list page)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
+})
+export class BookListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
