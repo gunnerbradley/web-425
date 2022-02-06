9d7ecb1e245d4fa3e5b0025334a96d944d2c4c6a
Gunner Bradley <gunnerbradley.dev@gmail.com>
2022-02-06 17:00:14 -0600

    Assignment 5.4 - Dialogs

---
+++ week-5/in-n-out-booksp3/src/app/contact/contact.component.ts
@@ -0,0 +1,19 @@
+/**
+ * Title: contact.component.ts
+ * Author: Gunner Bradley
+ * Date: February 6th 2022
+ * Description: Contact component (contact page)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+})
+export class ContactComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
